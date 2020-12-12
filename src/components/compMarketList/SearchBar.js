217c3a72321613cc16c9c2504b433530723246b9
FellypeQ <fellype.ads@gmail.com>
2020-12-12 14:20:46 -0300

    barra de pesquisa melhorada

--- src/components/compMarketList/SearchBar.js
+++ src/components/compMarketList/SearchBar.js
@@ -1,44 +1,19 @@
 import React, { useState, useEffect } from "react";
 
+import Autocomplete from "./Autocomplete";
+
 function SearchBar(props) {
   const [state, setState] = useState([]);
-  const [stateSrh, setStateSrh] = useState([]);
-  const [search, setSearch] = useState("");
 
   useEffect(() => {
     let arr = [];
-    props.list.map((list) =>
-      Object.values(list).map((cat) => cat.map((prod) => arr.push(prod)))
-    );
+    Object.values(props.list).map((cat) => cat.map((prod) => arr.push(prod)));
     setState(arr);
-    setStateSrh(arr);
   }, [props]);
 
-  function handleChange(event) {
-    setSearch(event.currentTarget.value);
-    setState(
-      stateSrh.filter((el) =>
-        el.toLowerCase().includes(event.currentTarget.value.toLowerCase())
-      )
-    );
-  }
-
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Procure o produto que deseja"
-        className="searchBar"
-        value={search}
-        onChange={handleChange}
-      />
-      <div className="productList">
-        {state.map((prod, idx) => (
-          <p key={idx} className="btn">
-            {prod}
-          </p>
-        ))}
-      </div>
+    <div className="searchCamp">
+      <Autocomplete options={state.map((prod) => prod)} />
     </div>
   );
 }
