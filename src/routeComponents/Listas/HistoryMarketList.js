6797f6f01d40e5939af28b8b9193799d86d19437
Vitor Machado Vieira <vmvieira55@gmail.com>
2020-12-16 10:18:40 -0300

    sorted lists

--- src/routeComponents/Listas/HistoryMarketList.js
+++ src/routeComponents/Listas/HistoryMarketList.js
@@ -27,81 +27,86 @@ function HistoryMarketList() {
   function renderAccordion() {
     if (loading === false) {
       return (
-        <Accordion defaultActiveKey="0">
-          {lists.map((list, idx) => {
-            return (
-              <Card key={idx}>
-                <Card.Header>
-                  <Accordion.Toggle
-                    as={Button}
-                    variant="link"
-                    eventKey={`${idx}`}
-                  >
-                    Lista {`${idx + 1}`}
-                    <Link className="mx-3 btn  login" to={`/menus/${list._id}`}>
-                      Editar Lista
-                    </Link>
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey={`${idx}`}>
-                  <Card.Body>
-                    <p className="font-weight-bold">Despensa</p>
-                    <ul>
-                      {list.Lista[0].Despensa.map((eachItem, i) => {
-                        return (
-                          <li key={i}>
-                            {eachItem.produto} - {eachItem.detalhes}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                    <p className="font-weight-bold">Freezer</p>
-                    <ul>
-                      {list.Lista[1].Freezer.map((eachItem, i) => {
-                        return (
-                          <li key={i}>
-                            {eachItem.produto} - {eachItem.detalhes}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                    <p className="font-weight-bold">Geladeira</p>
-                    <ul>
-                      {list.Lista[2].Geladeira.map((eachItem, i) => {
-                        return (
-                          <li key={i}>
-                            {eachItem.produto} - {eachItem.detalhes}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                    <p className="font-weight-bold">Frutas e Hortaliças</p>
-                    <ul>
-                      {list.Lista[3]["Frutas e Hortaliças"].map(
-                        (eachItem, i) => {
+        <Accordion defaultActiveKey={`${lists.length - 1}`}>
+          {lists
+            .map((list, idx) => {
+              return (
+                <Card key={idx}>
+                  <Card.Header>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant="link"
+                      eventKey={`${idx}`}
+                    >
+                      Lista {`${idx + 1}`}
+                      <Link
+                        className="mx-3 btn  login"
+                        to={`/menus/${list._id}`}
+                      >
+                        Editar Lista
+                      </Link>
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey={`${idx}`}>
+                    <Card.Body>
+                      <p className="font-weight-bold">Despensa</p>
+                      <ul>
+                        {list.Lista[0].Despensa.map((eachItem, i) => {
                           return (
                             <li key={i}>
                               {eachItem.produto} - {eachItem.detalhes}
                             </li>
                           );
-                        }
-                      )}
-                    </ul>
-                    <p className="font-weight-bold">Higiene</p>
-                    <ul>
-                      {list.Lista[4].Higiene.map((eachItem, i) => {
-                        return (
-                          <li key={i}>
-                            {eachItem.produto} - {eachItem.detalhes}
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </Card.Body>
-                </Accordion.Collapse>
-              </Card>
-            );
-          })}
+                        })}
+                      </ul>
+                      <p className="font-weight-bold">Freezer</p>
+                      <ul>
+                        {list.Lista[1].Freezer.map((eachItem, i) => {
+                          return (
+                            <li key={i}>
+                              {eachItem.produto} - {eachItem.detalhes}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      <p className="font-weight-bold">Geladeira</p>
+                      <ul>
+                        {list.Lista[2].Geladeira.map((eachItem, i) => {
+                          return (
+                            <li key={i}>
+                              {eachItem.produto} - {eachItem.detalhes}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      <p className="font-weight-bold">Frutas e Hortaliças</p>
+                      <ul>
+                        {list.Lista[3]["Frutas e Hortaliças"].map(
+                          (eachItem, i) => {
+                            return (
+                              <li key={i}>
+                                {eachItem.produto} - {eachItem.detalhes}
+                              </li>
+                            );
+                          }
+                        )}
+                      </ul>
+                      <p className="font-weight-bold">Higiene</p>
+                      <ul>
+                        {list.Lista[4].Higiene.map((eachItem, i) => {
+                          return (
+                            <li key={i}>
+                              {eachItem.produto} - {eachItem.detalhes}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              );
+            })
+            .reverse()}
         </Accordion>
       );
     }
