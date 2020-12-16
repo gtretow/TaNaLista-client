bbfe52703015fa639d113e4624b908be97823fd1
Vitor Machado Vieira <vmvieira55@gmail.com>
2020-12-16 15:43:10 -0300

    Delete CRUD

--- src/routeComponents/Listas/ListaDelete.js
+++ src/routeComponents/Listas/ListaDelete.js
@@ -1,22 +1,23 @@
 import React, { useEffect } from "react";
 import api from "../../apis/api";
+import { useHistory } from "react-router-dom";
 
 function ListaDelete(props) {
-  const { id } = props.match.params;
+  let history = useHistory();
 
   useEffect(() => {
     async function deleteLista() {
       try {
-        await api.delete(`/api/lista/${id}`);
-        props.history.push("/historico");
+        await api.delete(`/lista/${props.match.params.id}`);
+        history.push("/menus/listas-salvas");
       } catch (err) {
         console.error(err);
       }
     }
     deleteLista();
-  }, [id, props.history]);
+  }, []);
 
-  return <div>Deleting...</div>;
+  return <div></div>;
 }
 
 export default ListaDelete;
